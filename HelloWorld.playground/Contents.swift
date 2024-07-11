8b2260fa4a6934a015aa8b0e2100d8034b93e678
KS <kyusang.dev@gmail.com>
2024-07-11 16:31:07 +0900

    fizzBuzz(_:) 첫번째 구현

--- HelloWorld.playground/Contents.swift
+++ HelloWorld.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func fizzBuzz(_ number: Int) -> String {
+     let divisibleBy3 = number % 3 == 0
+     let divisibleBy5 = number % 5 == 0
+     switch (divisibleBy3, divisibleBy5) {
+     case (false, false): return "\(number)"
+     case (true, false): return "fizz"
+     case (false, true): return "buzz"
+     case (true, true): return "fizz-buzz"
+     }
+ }
